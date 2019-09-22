5bfdfd071e780beb6e0cd2c8fcd3ac149533ff7d
Martín Stefanelli <martin.stefanelli.96@hotmail.com>
2019-09-22 02:47:25 -0300

    Delete condicion_problema.py

--- Segundo x/condicion_problema.py
+++
@@ -1,7 +0,0 @@
-from funciones_en_comun import obtener_condicion_problema
-
-def condicion_problema(imprimir = True):
-    cp = obtener_condicion_problema()
-    print( "La condicion del problema es: {}".format(cp) )
-
-condicion_problema()
