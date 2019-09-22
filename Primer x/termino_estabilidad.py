0877be5a9b4016db13f731a152462dcfbc7b1d86
Martín Stefanelli <martin.stefanelli.96@hotmail.com>
2019-09-22 02:46:55 -0300

    Delete termino_estabilidad.py

--- Primer x/termino_estabilidad.py
+++
@@ -1,7 +0,0 @@
-from funciones_en_comun import obtener_termino_estabilidad
-
-def termino_estabilidad():
-    te, mu_simple = obtener_termino_estabilidad()
-    print( "El termino de establidad es: {}".format(te) )
-
-termino_estabilidad()
