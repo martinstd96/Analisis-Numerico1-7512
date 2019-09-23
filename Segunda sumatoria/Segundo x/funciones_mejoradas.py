dfca02b4c944af8b821f06ac97039c305aeb730f
jpdicomo <jpdicomo@live.com>
2019-09-22 22:30:48 -0300

    Cambiada la forma en sumar cada termino en la segunda sumatoria

--- Segunda sumatoria/Segundo x/funciones_mejoradas.py
+++ Segunda sumatoria/Segundo x/funciones_mejoradas.py
@@ -4,16 +4,16 @@ import math
 def obtener_sumatoria_mejorada(x, n):
     suma = 0
 
-    for i in range(n, 0,-2):
+    for i in range(1, n + 1,2):
         y = (-1/4) ** i
         z = x ** (2 * i)
         u = ( math.factorial(i) ) ** 2
         v = z / u
         k = y * v
         #-------------------------
-        y_sig =  (-1 / 4) ** (i-1) 
-        z_sig =  x ** (2 * (i-1)) 
-        u_sig =  ( math.factorial(i-1) ) ** 2 
+        y_sig =  (-1 / 4) ** (i+1) 
+        z_sig =  x ** (2 * (i+1)) 
+        u_sig =  ( math.factorial(i+1) ) ** 2 
         v_sig =  z_sig / u_sig 
         sig =  y_sig * v_sig 
 
@@ -36,16 +36,16 @@ def obtener_sumatoria_doble_mejorada():
     x = np.float64( 100488 * ( 10 ** (-4) ) + 102889 * ( 10 ** (-5) ) )
     suma = np.float64(0)
 
-    for i in range(25, 0 ,-2):
+    for i in range(1, 25 + 1  ,2):
         y = np.float64( (-1 / 4) ** i )
         z = np.float64( x ** (2 * i) )
         u = np.float64( ( math.factorial(i) ) ** 2 )
         v = np.float64( z / u )
         k = np.float64( y * v )
         #----------------------
-        y_sig = np.float64( (-1 / 4) ** (i-1) )
-        z_sig = np.float64( x ** (2 * (i-1)) )
-        u_sig = np.float64( ( math.factorial(i-1) ) ** 2 )
+        y_sig = np.float64( (-1 / 4) ** (i+1) )
+        z_sig = np.float64( x ** (2 * (i+1)) )
+        u_sig = np.float64( ( math.factorial(i+1) ) ** 2 )
         v_sig = np.float64( z_sig / u_sig )
         sig = np.float64( y_sig * v_sig )
 
@@ -57,25 +57,24 @@ def obtener_sumatoria_simple_mejorada():
     x = np.float32( 100488 * ( 10 ** (-4) ) + 102889 * ( 10 ** (-5) ) )
     suma = np.float32(0)
 
-    for i in range(25, 0 ,-2):
+    for i in range(1, 25 +1  ,2):
         y = np.float32( (-1 / 4) ** i )
-
         if i > 18: #con i >= 19 explota si no hacemos asi
             v = factorizar(x, i, 2 * i)
-            v_sig = factorizar(x , i-1 , 2*(i-1))
+            v_sig = factorizar(x , i-1 , 2*(i+1))
 
         else:
             z = np.float32( x ** (2 * i) )
             u = np.float32( ( math.factorial(i) ) ** 2 )
             v = np.float32( z / u )
             #-----------------------
-            z_sig = np.float32( x ** (2 * (i-1)) )
-            u_sig = np.float32( ( math.factorial(i-1) ) ** 2 )
+            z_sig = np.float32( x ** (2 * (i+1)) )
+            u_sig = np.float32( ( math.factorial(i+1) ) ** 2 )
             v_sig = np.float32( z_sig / u_sig )
 
         k = np.float32( y * v )
         #----------------------------
-        y_sig = np.float32( (-1 / 4) ** (i-1) )
+        y_sig = np.float32( (-1 / 4) ** (i+1) )
         
         sig = np.float32( y_sig * v_sig )
 
