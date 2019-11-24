ab681a5cdd95854cf94d14824f1db4fb09d299d7
Martín Stefanelli <martin.stefanelli.96@hotmail.com>
2019-11-24 15:21:09 -0300

    ïtem 4 funcionando correctamente

--- tp2/resolver_por_euler_runge_kutta2.py
+++ tp2/resolver_por_euler_runge_kutta2.py
@@ -10,7 +10,7 @@ def resolver_por_euler_runge_kutta2():
     semilla_u = 1
     semilla_v = 10
     h = 1.6 * ( 10 ** (-5) )
-    tol = 1 * ( 10 ** (-6) )
+    tol = 1 * ( 10 ** (-4) )
 
     while t < 1:
         euler_u = semilla_u + ( h * funcion_uno(semilla_v) )
@@ -19,14 +19,10 @@ def resolver_por_euler_runge_kutta2():
         r_k_v = semilla_v + (h / 2) * ( funcion_dos(semilla_u, semilla_v) + funcion_dos(euler_u, euler_v) )
         error_local_u = abs(r_k_u - euler_u)
         error_local_v = abs(r_k_v - euler_v)
-        error_local_u /= h
-        error_local_v /= h
         lista_auxiliar = [error_local_u, error_local_v]
-        print(error_local_u, error_local_v)
-        lista_auxiliar.sort()
-        error_local = lista_auxiliar[0]
+        error_local = min(lista_auxiliar)
 
-        if error_local < tol:
+        if (error_local / h) < tol:
             semilla_u = r_k_u
             semilla_v = r_k_v
             lista_u.append(semilla_u)
@@ -34,13 +30,21 @@ def resolver_por_euler_runge_kutta2():
             lista_t.append(t)
             t += h
 
-        hp = h
-        v = 1 * ( 10 ** (-2) )
-        u = 1 * ( 10 ** (-15) )
-        h = 0.8 * h * ((tol * h) / error_local)
+        h_previo = h
+        maximo_valor_h = 1.967 * ( 10 ** (-5) )
+        minimo_valor_h = 1 * ( 10 ** (-15) )
 
-        if (h < hp/2 or h > 2*hp) and (h > v or h < u):
-            print(h)
+        if error_local == 0: h /= 2
+
+        else: h = 0.8 * h * ((tol * h) / error_local)
+
+        if h < (h_previo / 2): h = h_previo / 2
+
+        if h > (h_previo * 2): h = h_previo * 2
+
+        if h < minimo_valor_h: h = minimo_valor_h
+
+        if h > maximo_valor_h: h = maximo_valor_h
 
     mostrar(lista_u, lista_v, lista_t)
 
